67830ed4f8bc1462f9fd14da9832808bfaf67609
Christian M. Abuda <cabuda_230000001609@uic.edu.ph>
2024-10-21 09:32:09 +0800

    Update mode.js

--- mode.js
+++ mode.js
@@ -2,21 +2,19 @@ let lightmode = localStorage.getItem('light-mode');
 const mode = document.getElementById('mode');
 
 const enablelightmode = () => {
-    document.body.classList.add('lightmode'); // Add the correct class name
+    document.body.classList.add('lightmode'); 
     localStorage.setItem('light-mode', 'active');
 };
 
 const disablelightmode = () => {
-    document.body.classList.remove('lightmode'); // Add the correct class name
+    document.body.classList.remove('lightmode'); 
     localStorage.setItem('light-mode', 'null');
 };
 
-// Check the saved preference and apply it on page load
 if (lightmode === 'active') {
     enablelightmode();
 }
 
-// Add event listener to the theme switch button
 mode.addEventListener('click', () => {
     lightmode = localStorage.getItem('light-mode');
     lightmode !== 'active' ? enablelightmode() : disablelightmode();
